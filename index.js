51afa9f00d8f2e4ce4d5d09d11a6905000fdb200
Mikey <Mikey.Stengel@gmx.de>
2018-09-05 14:06:40 +0200

    Type: fix ^--^  ^------------^ |     | |     +-> Respect whole names. | +-------> clientID should turn into client_id_ instead of client_i_d. Added tests and got rid of unneeded packages.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-'use strict'
+"use strict";
 
-var map = require('map-obj')
-var snakeCase = require('to-snake-case')
+var map = require("map-obj");
 
-module.exports = function (obj, options) {
-  options = Object.assign({deep: true}, options)
+module.exports = function(obj, options) {
+  options = Object.assign({ deep: true }, options);
 
-  return map(obj, function (key, val) {
-    return [snakeCase(key), val]
-  }, options)
-}
+  return map(
+    obj,
+    (key, val) => {
+      // respects whole words being uppercase e.g turns clientID into client_id
+      // instead of client_i_d. See https://stackoverflow.com/a/30521308
+      const snakeCasedKey = key
+        .replace(/\.?([A-Z]+)/g, (x, y) => {
+          return "_" + y.toLowerCase();
+        })
+        .replace(/^_/, "");
+      return [snakeCasedKey, val];
+    },
+    options
+  );
+};
--- test.js
+++ test.js
@@ -1,17 +1,27 @@
-'use strict'
+"use strict";
 
-var test = require('tape')
-var Snake = require('./')
+var test = require("tape");
+var Snake = require("./");
 
-test(function (t) {
-  t.deepEqual(Snake({fooBar: 'baz', nested: {fooBar: 'baz'}}), {foo_bar: 'baz', nested: {foo_bar: 'baz'}})
-  t.end()
-})
+test(function(t) {
+  t.deepEqual(Snake({ fooBar: "baz", nested: { fooBar: "baz" } }), {
+    foo_bar: "baz",
+    nested: { foo_bar: "baz" }
+  });
+  t.end();
+});
 
-test('shallow conversion with {deep: false}', function (t) {
-  t.deepEqual(
-    Snake({fooBar: {barBaz: 'qux'}}, {deep: false}),
-    {foo_bar: {barBaz: 'qux'}}
-  )
-  t.end()
-})
+test("Respects whole words", t => {
+  t.deepEqual(Snake({ fooBAR: "bazBAR", nested: { fooBAR: "bazID" } }), {
+    foo_bar: "bazBAR",
+    nested: { foo_bar: "bazID" }
+  });
+  t.end();
+});
+
+test("shallow conversion with {deep: false}", function(t) {
+  t.deepEqual(Snake({ fooBar: { barBaz: "qux" } }, { deep: false }), {
+    foo_bar: { barBaz: "qux" }
+  });
+  t.end();
+});
