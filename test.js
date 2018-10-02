2de81142973ebe086416ab369187afc0a20067a3
MrKey <mikey.stengel96@gmail.de>
2018-10-02 03:45:09 +0200

    Type: test ^--^  ^------------^ |     | |     +-> Added more test cases to make sure snakeCasing keys works when the object key contains numbers | +-------> Making sure that the object keys 'fooBAR1' or 'fooBar1' yield foo_bar1. This works for nested and unnested object keys.

--- test.js
+++ test.js
@@ -19,6 +19,22 @@ test("Respects whole words", t => {
   t.end();
 });
 
+test("Doesn't add an underscore before a number", t => {
+  t.deepEqual(Snake({ fooBar1: "bazBar1", nested: { fooBar1: "bazId1" } }), {
+    foo_bar1: "bazBar1",
+    nested: { foo_bar1: "bazId1" }
+  });
+  t.end();
+});
+
+test("Doesn't add an underscore before number after whole word", t => {
+  t.deepEqual(Snake({ fooBAR1: "bazBAR1", nested: { fooBAR1: "bazID1" } }), {
+    foo_bar1: "bazBAR1",
+    nested: { foo_bar1: "bazID1" }
+  });
+  t.end();
+});
+
 test("shallow conversion with {deep: false}", function(t) {
   t.deepEqual(Snake({ fooBar: { barBaz: "qux" } }, { deep: false }), {
     foo_bar: { barBaz: "qux" }
